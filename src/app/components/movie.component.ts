a6927dac6344448120074aef6854f2ad1bff7d7b
Joel <joeltwaage@gmail.com>
2017-04-19 16:11:44 -0700

    tutorial through ngModel, just before API services

--- src/app/components/movie.component.ts
+++ src/app/components/movie.component.ts
@@ -8,20 +8,31 @@ import { Component } from '@angular/core';
     <div *ngIf="show2016">
       <h3>Movies 2016:</h3>
       <ol>
-        <li *ngFor="let movie of movies2016">
-          {{movie}}
-        </li>
+        <li *ngFor="let movie of movies2016">{{movie}}</li>
+        <li>{{missing2016}}</li>
       </ol>
+      <form>
+        <label>Number 11: </label>
+        <br>
+        <input type="text" name="missing2016" [(ngModel)]="missing2016"/>
+      </form>
     </div>
+
+
+
     <hr>
     <button (click)="toggle2015()">{{show2015 ? "Hide 2015" : "Show 2015"}}</button>
     <div *ngIf="show2015">
       <h3>Movies 2015:</h3>
       <ol>
-        <li *ngFor="let movie of movies2015">
-          {{movie}}
+        <li *ngFor="let movie of movies2015; let i = index">
+          {{movie}} <button (click)="deleteMovie2015(i)">X</button>
         </li>
       </ol>
+      <form (submit)="addMovie2015(movie2015.value)">
+        <label>Add 2015 Movie: </label><br>
+        <input type="text" #movie2015 /><br>
+      </form>
     </div>
   `,
 })
@@ -29,12 +40,14 @@ export class MovieComponent  {
   name: string;
   movies2016: string[];
   movies2015: string[];
+  missing2016: string;
   show2015: boolean;
 
   constructor(){
     this.name = 'Joel';
     this.email = 'joeltwaage@gmail.com';
     this.movies2016 = ['Moonlight','The VVitch','Elle','The Lobster','Jackie','Mancheego by the Cheese','OJ: Made in America','A Monster Calls','Hunt for the Wilderpeople','Captain Fantastic'];
+    this.missing2016='',
     this.movies2015 = ['Mad Max: Fury Road','Spotlight','Spy','Carol','Hateful 8','Anomalisa','Steve Jobs','Inside Out','The Martian','Sicario'];
     this.show2016 = false;
     this.show2015 = false;
@@ -54,5 +67,11 @@ export class MovieComponent  {
     } else {
       this.show2015 = true;
     }
+  }
+  addMovie2015(movie2015){
+    this.movies2015.push(movie2015);
+  }
+  deleteMovie2015(i){
+    this.movies2015.splice(i, 1);
   }
 }
